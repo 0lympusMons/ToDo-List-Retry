c5d92c64678fbc880270256a1fc49b27e6d8283d
0lympusMons <84634539+0lympusMons@users.noreply.github.com>
2023-08-30 22:27:40 +0800

    Store today's task (Storage)

--- src/modules/Storage.js
+++ src/modules/Storage.js
@@ -1,16 +1,30 @@
-export default class Storage{
+import Dates from "./Dates";
+
+export default class Storage {
 
     // each index contains object: {title, date, priority}
     static inboxStorage = [];
     static todayStorage = [];
 
-    static addTask(to, newTask){
-        if(to == "inbox"){
+    static addTask(to, newTask) {
+        if (to == "inbox") {
             Storage.inboxStorage.push(newTask);
             console.table(Storage.inboxStorage);
         }
+
+        //Warning: function not following single-responsibility principle
+        //because it performs filtering of today's tasks
+
+        // Date.isToday(2023, 07, 29) or Date.isToday(year, month, day)
+        //If task date is today, then add it to todayStorage
+        Storage.todayStorage = Storage.inboxStorage.filter((task) => {
+            let date = Dates.convertDate(task.date);
+            
+            return Dates.isToday(new Date(date.year, date.month, date.day));
+        });
+        console.log("___________________________");
+        console.table(Storage.todayStorage);
     }
 
-    todayStorage = inboxStorage.filter((task) => task.date );
-    
+
 }
