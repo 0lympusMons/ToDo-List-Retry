cbb1af0b43fa08820e7e348df5e096b8d0f7d87a
0lympusMons <84634539+0lympusMons@users.noreply.github.com>
2023-08-30 22:22:06 +0800

    Add func convertDate
    Converts string date to numbers

--- src/modules/Dates.js
+++ src/modules/Dates.js
@@ -2,12 +2,29 @@ import { isToday as _isToday, isThisWeek as _isThisWeek } from 'date-fns';
 
 export default class Dates {
 
+    ////TODAY
     static isToday(date) {
         return _isToday(date);
     }
 
-    static isThisWeek(date){
+    ////THIS WEEK
+    static isThisWeek(date) {
         return _isThisWeek(date);
+    }
+
+
+    ////CONVERTER
+
+    //convert string date to int
+    //example
+    static convertDate(stringDate) {
+
+        let dateArr = stringDate.split('-');
+        let year = parseInt(dateArr[0]);
+        let month = dateArr[1] - 1;
+        let day = parseInt(dateArr[2]);
+
+        return { year, month, day };
     }
 };
 
