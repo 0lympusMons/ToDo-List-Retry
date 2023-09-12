d0f7a70fa1a4ee15698f552700d4fd779cf72aea
0lympusMons <84634539+0lympusMons@users.noreply.github.com>
2023-09-12 21:36:10 +0800

    Add changes

--- src/modules/Storage.js
+++ src/modules/Storage.js
@@ -15,15 +15,86 @@ export default class Storage {
         //Warning: function not following single-responsibility principle
         //because it performs filtering of today's tasks
 
-        // Date.isToday(2023, 07, 29) or Date.isToday(year, month, day)
-        //If task date is today, then add it to todayStorage
-        Storage.todayStorage = Storage.inboxStorage.filter((task) => {
-            let date = Dates.convertDate(task.date);
-            
+    static get inbox() {
+
+        return {
+            title: "Inbox",
+            tasks: Storage.#inboxStorage,
+            taskKeyCounter: 0,
+            addTask: (task) => {
+                task.key = Storage.inboxKeyCounter.newKey();
+                Storage.addTaskToStorage(task, Storage.#inboxStorage)
+            },
+
+        };
+    }
+
+    static get todayTasks() {
+        // Filter inbox tasks based on whether they are for today
+        //⚠️⚠️ Clean code
+        Storage.#todayStorage = Storage.#inboxStorage.filter((task) => {
+            const date = Dates.convertDate(task.date);
             return Dates.isToday(new Date(date.year, date.month, date.day));
         });
-        console.log("___________________________");
-        console.table(Storage.todayStorage);
+
+        Storage.projectsStorage.forEach(project => {
+            let tasksToday = project.tasks.filter((task)=>{
+                const date = Dates.convertDate(task.date);
+                return Dates.isToday(new Date(date.year, date.month, date.day));
+            });
+
+            tasksToday.forEach(task => {
+                Storage.#todayStorage.push(task);
+            });
+        });
+
+        // ⚠️⚠️ Clean code ends here
+
+        return {
+            title: "Today",
+            tasks: Storage.#todayStorage,
+            addTask: (task) => Storage.addTaskToStorage(task, Storage.#todayStorage)
+        };
+    }
+
+    static get thisWeekTasks() {
+        // Filter inbox tasks based on whether they are for this week
+        Storage.#thisWeekStorage = Storage.#inboxStorage.filter((task) => {
+            const date = Dates.convertDate(task.date);
+            return Dates.isThisWeek(new Date(date.year, date.month, date.day));
+        });
+
+        Storage.projectsStorage.forEach(project => {
+            let tasksThisWeek = project.tasks.filter((task)=>{
+                const date = Dates.convertDate(task.date);
+                return Dates.isThisWeek(new Date(date.year, date.month, date.day));
+            });
+
+            tasksThisWeek.forEach(task => {
+                Storage.#thisWeekStorage.push(task);
+            });
+        });
+
+        return {
+            title: "This Week",
+            tasks: Storage.#thisWeekStorage,
+            addTask: (task) => Storage.addTaskToStorage(task, Storage.#thisWeekStorage)
+        };
+    }
+
+    static get projects() {
+        return Storage.projectsStorage;
+    }
+
+    static addProject(newProject) {
+        Storage.projectsStorage.push(newProject);
+    }
+
+    //gets project inside Storage.projectsStorage
+    //Parameter: key
+    //example: getProject(1), gets project with key 1
+    static getProject(key) {
+        return Storage.projectsStorage.find((project => project.key === key));
     }
 
 
