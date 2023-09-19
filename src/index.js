e53eaad353981f8dfb49e1fad4a4a0363bdf7e6e
0lympusMons <84634539+0lympusMons@users.noreply.github.com>
2023-09-20 07:13:31 +0800

    Improve UI

--- src/index.js
+++ src/index.js
@@ -2,8 +2,5 @@ import Events from "./modules/Event";
 import UI from "./modules/UI";
 import Storage from "./modules/Storage";
 
-UI.initProjectButtons();
-UI.loadHomePage();
-
-//this is for event listeners to work
 Storage.initialize();
+UI.loadHomePage();
